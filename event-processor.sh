191113c41e029408446ea3f79887ecba93db727d
David <david.caicedo@cleteci.com>
2017-10-24 21:20:07 -0400

    shell script

---
+++ event-processor.sh
@@ -0,0 +1,7 @@
+result=`ps aux | grep -i "event-processor.js $1" | grep -v "grep" | wc -l`
+if [ $result -ge 1 ]
+   then
+        echo "script is running"
+   else
+        /home/ec2-user/node-v6.9.1-linux-x64/bin/node /home/ec2-user/ralentage-events/event-processor.js $1
+fi
